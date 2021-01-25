f02540353e1c3dc26ca78c7cb86205705b3171eb
Israel Lima <israel.lima@ceuma.br>
2021-01-25 11:38:57 -0300

    feat: add event table and custom admin site for it

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import Event
+
+
+class AdminEvent(admin.ModelAdmin):
+    list_display = ('title', 'date', 'created_at')
+    list_filter = ('title',)
+
+
+admin.site.register(Event, AdminEvent)
--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,7 +7,14 @@ from django.db import models
 class Event(models.Model):
     title = models.CharField(max_length=70)
     description = models.TextField(blank=True, null=True)
-    date = models.DateTimeField()
+    date = models.DateTimeField(verbose_name="Data do evento")
     created_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'event'
+
+    def __str__(self):
+        return self.title
 
 
